f6e724342477c2cf9d38e5ca5401a7c5480aac7c
tobi <130551183+Do-Trung-Quan@users.noreply.github.com>
2025-03-11 22:01:51 +0700

    Delete Thuy + DucMinh directory

--- Thuy + DucMinh/editButton.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".editIcon").forEach(icon => {
-    icon.addEventListener("click", function() {
-        let url = this.dataset.url; // Lấy URL từ data-url
-        window.open(url, "_blank");
-    });
-});
